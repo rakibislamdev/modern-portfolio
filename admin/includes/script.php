a6f562dd90b1cdecf49d2e1d10503c715d423133
RAKIB <rakibislamdev@gmail.com>
2022-04-09 11:17:16 +0600

    separate all part done

---
+++ admin/includes/script.php
@@ -0,0 +1,8 @@
+<script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendors/apexcharts/apexcharts.js"></script>
+<script src="assets/js/pages/dashboard.js"></script>
+<script src="assets/js/mazer.js"></script>
+</body>
+
+</html>
